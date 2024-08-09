d3626acf3f379b251ed81e288feb1a15cad4680e
galileor97 <44834772+galileor97@users.noreply.github.com>
2024-08-09 11:14:09 +0700

    Update api.js

--- src/helper/api.js
+++ src/helper/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // import.meta.env.VITE_BASE_URL
 const api = axios.create({
-    baseURL: 'http://34.229.241.54'
+    baseURL: 'https://potret.galileor.com'
 });
 
 export default api;
